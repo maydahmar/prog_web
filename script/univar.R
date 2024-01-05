7de85bf402a3510209ea4b4d820ed75d623e95de
Hanane BENNOUI <bennoui.hannane@gmail.com>
2024-01-05 17:34:39 +0100

    v2 uni var

--- script/univar.R
+++ script/univar.R
@@ -1,39 +1,43 @@
-#-------------- Uni var categ ---------------
-# fonction visualiser les variable 
+library(tidyverse)
+library(plotly)
+
+
 button.to.remove <- c("zoom", "pan", "select", "zoomIn", "zoomOut", "autoScale", "resetScale", "lasso2d", "hoverClosestCartesian", "hoverCompareCartesian")
 
-#------------- Uni var analysis -----------
+#------------- analyse Uni var  -----------
 
-#------------- Numerical Var ----------
-
-#-------------par variable -------------------------
+#-------------  var Numeri ----------
 
 
+get.boxplot <- function(dataset, x.name){
+  if (x.name == ""){
+    return(NULL)
+  }
+  #cree 
+  title <- paste0("Variance of ", x.name)
+  fig <- plot_ly(dataset , y = dataset[[x.name]], type = "box")
+  fig <- layout(fig, xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE), yaxis = list(title = list(text = x.name)), title = list(text = title))
+  return(config(fig, modeBarButtonsToRemove = button.to.remove, displaylogo = F))
+}
 
-visua_univar_data <- fonction(df,output){
-  for(col_name in names(df)){
-    if (is.numeric(df[[col_name]])|| is.character(df[[col_name]])) {
-      
-      # Calcul des effectifs
-      effectifs <- reactive({table(data())})
-      
-      # Diagramme en colonnes
-      output$colonnes <- renderPlot({
-        barplot(df(), main = "Catégories ", 
-                ylab="Effectifs", las = 2,
-                names.arg = substr(names(df()), 1, 4))
-        
-      })
-      
-      # Diagramme en secteurs
-      output$secteurs <- renderPlot({
-        pie(df(), labels = substr(names(df()), 1, 4), 
-            main = "Catégories Socioprofessionnelles", col=c())
-      })
-    }
-      
-      
-} }
+get.histo <- function(dataset, x.name){
+  fig <- plot_ly(dataset, y = dataset[[x.name]], name = "", type = "histogram")
+  title <- paste0("Histo of ", x.name)
+  fig <- layout(fig, yaxis = list(title = list(text = x.name)), title = list(text = title))
+  return(config(fig, modeBarButtonsToRemove = button.to.remove, displaylogo = F))
+}
+
+#--------- Categorielle  Var ----------
+
+get.pie <- function(dataset, x.name){
+  fig <- plot_ly(dataset, labels = dataset[[x.name]], type = 'pie')
+  fig <- fig %>% layout(title = paste0("Proportion de  ", x.name), 
+                        xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
+                        yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))
+  return(config(fig, modeBarButtonsToRemove = button.to.remove, displaylogo = F))
+}
+
+
 
 
 
