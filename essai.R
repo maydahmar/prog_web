ae12114cdc95e6abd237f819faa69dd0682c20cf
maydahmar <maydahmar2910@gmail.com>
2024-01-06 18:14:51 +0100

    regression faite

--- essai.R
+++ essai.R
@@ -3,6 +3,10 @@ library(shinydashboard)
 library(FactoMineR)
 library(factoextra)
 library(rpart)
+library(caret)
+library(randomForest)
+
+
 
 # Fonction de normalisation
 normalizeData <- function(column) {
@@ -52,15 +56,28 @@ ui <- dashboardPage(
       ),
       tabPanel("Modèles de Classification",
                fluidRow(
-                 box(title = "Entraînement des Modèles",
-                     selectInput("selectedModel", "Choisir le Modèle",
-                                 choices = c("Régression Logistique" = "lm", "Arbre de Décision" = "rpart")),
-                     actionButton("trainModels", "Entraîner le Modèle")
+                 column(6,
+                        box(title = "Entraînement des Modèles",
+                            selectInput("selectedModel", "Choisir le Modèle",
+                                        choices = c("Régression Linéaire" = "lm", 
+                                                    "Arbre de Décision" = "rpart", 
+                                                    "Random Forest" = "rf",
+                                                    "Régression Ridge" = "ridge")),
+                            actionButton("trainModels", "Entraîner le Modèle")
+                        )
                  ),
-                 box(title = "Évaluation Comparative",
-                     plotOutput("modelComparisonPlot")),
-                 box(title = "Résultats",
-                     verbatimTextOutput("modelResultsText"))
+                 column(6,
+                        box(title = "Évaluation Comparative",
+                            plotOutput("modelComparisonPlot"))
+                 )
+               ),
+               fluidRow(
+                 column(12,
+                        box(title = "Résultats",
+                            verbatimTextOutput("modelResultsText"),
+                            plotOutput("scatterPlot")
+                        )
+                 )
                )
       )
     )
@@ -98,8 +115,10 @@ server <- function(input, output, session) {
     values$df_numeric <- df[values$numeric_vars]
     
     
+    values$df_dummified <- dummifyData(df)
+    
     # Calcul de l'ACP
-    values$acp_result <- PCA(values$df_numeric, graph = FALSE)
+    values$acp_result <- PCA(values$df_dummified, graph = FALSE)
     
     # Conversion des variables qualitatives en variables factices pour l'AFDM
     #df_afcm <- dummifyData(df)
@@ -194,11 +213,9 @@ server <- function(input, output, session) {
         if (is.numeric(values$data[[varName]])) {
           # Replace missing values with the mean for numeric variables
           values$data[[varName]][is.na(values$data[[varName]])] <- mean(values$data[[varName]], na.rm = TRUE)
-          cat('tot1')
         } else if (is.character(values$data[[varName]])) {
           # Replace missing values with the mode for character variables
           values$data[[varName]][is.na(values$data[[varName]])] <- Mode(values$data[[varName]])
-          cat('toto2')
         }
       }
     }
@@ -234,50 +251,111 @@ server <- function(input, output, session) {
   })
   
   # Reactive values for training models
-  trained_model <- reactiveValues()
+  trained_model <- reactiveValues(model = NULL, lm = NULL, rpart = NULL, rf = NULL, lr = NULL)
+  test <- reactiveValues(X_test = NULL, Y_test = NULL)
+  
   
   observeEvent(input$trainModels, {
-    req(input$file1, input$targetVariable, input$selectedModel)
-    
+    req(values$data, input$file1, input$targetVariable, input$selectedModel)
     # Séparer les données en variables explicatives (X) et variable cible (Y)
-    X_train <- values$df_numeric[, -which(names(values$df_numeric) == input$targetVariable, arr.ind = TRUE)]
-    Y_train <- values$df_numeric[, input$targetVariable]
+    X <- values$df_numeric[, -which(names(values$df_numeric) == input$targetVariable, arr.ind = TRUE)]
+    Y <- values$df_numeric[, input$targetVariable]
     
-    print(Y_train)
+    # Diviser les données en ensembles d'entraînement et de test (80% train, 20% test)
+    set.seed(123)  # Pour la reproductibilité
+    
+    splitIndex <- createDataPartition(Y, p = 0.8, list = FALSE)
+    X_train <- X[splitIndex, ]
+    Y_train <- Y[splitIndex]
+    X_test <- X[-splitIndex, ]
+    Y_test <- Y[-splitIndex]
     
     # Entraîner le modèle sélectionné
     if (input$selectedModel == "lm") {
-      model <- lm(Y_train ~ ., data = values$df_numeric)
+      model <- lm(Y_train ~ ., data = X_train)
+      trained_model$lm <- model
     } else if (input$selectedModel == "rpart") {
-      model <- rpart(Y_train ~ ., data = values$df_numeric, method = "anova")
+      model <- rpart(Y_train ~ ., data = X_train, method = "anova")
+      trained_model$rpart <- model
+    } else if (input$selectedModel == "rf") {
+      # Utiliser un modèle de forêt aléatoire
+      model <- randomForest(X_train, Y_train)
+      trained_model$rf <- model
+    } else if (input$selectedModel == "ridge") {
+      # Utiliser un modèle de régression ridge
+      model <- train(X_train, Y_train, method = "ridge")
+      trained_model$lr <- model
     }
-    
     
     # Stocker le modèle entraîné dans reactiveValues
     trained_model$model <- model
+    
+    # Stocker les données de test dans reactiveValues
+    test$X_test <- X_test
+    test$Y_test <- Y_test
   })
+  
   
   # Comparaison des modèles
   output$modelComparisonPlot <- renderPlot({
-    req(trained_model$model)
+    req(trained_model, test)
     
-    # Ajoutez votre logique pour la comparaison des modèles ici
-    # Vous pouvez utiliser des métriques comme la précision, le rappel, le F-score, ROC, AUC, etc.
-    # Exemple simplifié:
-    results <- confusionMatrix(predict(trained_model$model, newdata = X_train, type = "response"), Y_train)
-    plot(results)
+    # Liste pour stocker les métriques de performance de chaque modèle
+    metrics_list <- list()
+    
+    # Fonction pour calculer les métriques et les stocker dans la liste
+    calculate_metrics <- function(model, X_test, Y_test) {
+      predictions <- predict(model, newdata = X_test)
+      rmse <- sqrt(mean((Y_test - predictions)^2))
+      mae <- mean(abs(Y_test - predictions))
+      return(c(RMSE = rmse, MAE = mae))
+    }
+    
+    # Calculer les métriques pour chaque modèle et les stocker dans la liste
+    metrics_list[["Régression Linéaire"]] <- calculate_metrics(trained_model$lm, test$X_test, test$Y_test)
+    metrics_list[["Arbre de Décision"]] <- calculate_metrics(trained_model$rpart, test$X_test, test$Y_test)
+    metrics_list[["Random Forest"]] <- calculate_metrics(trained_model$rf, test$X_test, test$Y_test)
+    metrics_list[["Régression Ridge"]] <- calculate_metrics(trained_model$lr, test$X_test, test$Y_test)
+    
+    # Convertir la liste en un dataframe pour faciliter la manipulation des données
+    metrics_df <- as.data.frame(metrics_list)
+    
+    # Tracer un graphique à barres
+    barplot(t(metrics_df), beside = TRUE, col = rainbow(4), legend.text = rownames(metrics_df),
+            main = "Comparaison des Modèles", ylab = "Scores", xlab = "Métriques", ylim = c(0, max(unlist(metrics_list)) + 1))
+    legend("topright", legend = colnames(metrics_df), fill = rainbow(4))
   })
   
   # Afficher les résultats
   output$modelResultsText <- renderText({
-    req(trained_model$model)
+    req(trained_model$model, test)
     
-    # Ajoutez votre logique pour afficher les résultats ici
-    # Vous pouvez extraire les performances du modèle et afficher les métriques souhaitées
-    # Exemple simplifié:
-    results <- confusionMatrix(predict(trained_model$model, newdata = X_train, type = "response"), Y_train)
-    paste("Résultats de l'Entraînement du Modèle:\n", results)
+    results <- data.frame(
+      Observed = test$Y_test,
+      Predicted = predict(trained_model$model, newdata = test$X_test)
+    )
+    
+    rmse <- sqrt(mean((results$Observed - results$Predicted)^2))
+    mae <- mean(abs(results$Observed - results$Predicted))
+    
+    paste("Résultats de l'Entraînement du Modèle:\n", 
+          "RMSE:", rmse, "\n",
+          "MAE:", mae)
   })
+  
+  output$scatterPlot <- renderPlot({
+    req(trained_model$model, test$X_test, test$Y_test)
+    
+    predictions <- predict(trained_model$model, newdata = test$X_test)
+    
+    # Créer un nuage de points
+    plot(test$Y_test, predictions, main = "Nuage de points entre valeurs réelles et prédites",
+         xlab = "Valeurs réelles", ylab = "Prédictions")
+    
+    # Ajouter une ligne de référence pour une prédiction parfaite
+    abline(a = 0, b = 1, col = "red")
+  })
+  
 }
 
 
